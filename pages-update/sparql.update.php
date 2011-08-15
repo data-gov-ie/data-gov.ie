60cfbfe7f6509b020922943123d43aaa34210926
Sarven Capadisli <sarven.capadisli@deri.org>
2011-08-15 11:52:21 +0100

    Added owl prefix

--- pages-update/sparql.update.php
+++ pages-update/sparql.update.php
@@ -18,6 +18,7 @@ header ('Content-type:text/html; charset=utf-8');
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
 PREFIX dcterms: <http://purl.org/dc/terms/>
 PREFIX foaf: <http://xmlns.com/foaf/0.1/>
 PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
